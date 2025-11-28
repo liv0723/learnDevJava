805368cfa3ac7925a5fa593ca5fcffec2cab33f9
yan pizarro <pizarroestevez2@gmail.como>
2025-11-28 15:29:28 -0300

    Data type

---
+++ src/getting/know/language/java/languaje/basics/DataTypes.java
@@ -0,0 +1,16 @@
+package getting.know.language.java.languaje.basics;
+
+public class DataTypes {
+
+    byte typeByte = 127;
+    short typeShort = 32000;
+    int typeInt = 100000;
+    long typeLong = 100000000L;
+    float typeFloat = 3200000.0F;
+    double typeDouble = 10000000.0;
+    boolean typeBoolean = true;
+    char typeChar = 's';
+    String typeString = "Yan Pizarro";
+
+
+}
--- src/getting/know/language/obj/clas/inter/pack/BicycleDemo.java
+++ src/getting/know/language/obj/clas/inter/pack/BicycleDemo.java
@@ -11,14 +11,14 @@ public class BicycleDemo {
         // Invoke methods on
         //those objects
         bike1.changeCadence(50);
-        bike1.chanceGear(2);
+        bike1.changeGear(2);
         bike1.speedUp(4);
-        bike1.applyBreaks(4);
+        bike1.applyBrakes(4);
         bike1.printStates();
 
         bike2.changeCadence(23);
-        bike2.applyBreaks(3);
-        bike2.chanceGear(45);
+        bike2.applyBrakes(3);
+        bike2.changeGear(45);
         bike2.printStates();
     }
 }
